1a37733477be3a26a0f93cba5c1e425346838a18
ousiruon <ou.dehmani@gmail.com>
2024-09-23 19:23:44 -0400

    Games with no image

--- src/components/SortSelector.tsx
+++ src/components/SortSelector.tsx
@@ -1,5 +1,4 @@
 import { Button, Menu, MenuButton, MenuItem, MenuList } from "@chakra-ui/react";
-import { data } from "framer-motion/client";
 import { BsChevronDown } from "react-icons/bs";
 
 interface Props {
@@ -11,7 +10,7 @@ const SortSelector = ({onSelectSortOrder , sortOrder}: Props) => {
     { value: "", label: "Relevance" },
     { value: "-added", label: "Date added" },
     { value: "name", label: "Name" },
-    { value: "-released", label: "Release data" },
+    { value: "-released", label: "Release date" },
     { value: "-metacritic", label: "Popularity" },
     { value: "-rating", label: "Average" },
   ];
