3de2099f6477bd110bfd11177e9e377473bc7702
ousiruon <ou.dehmani@gmail.com>
2024-09-23 20:06:47 -0400

    Implement searching

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -16,6 +16,7 @@ const useGames = (gameQuery: GameQuery ) => useData<Game>('/games' , {params: {
   genres: gameQuery.genre?.id,
   platforms: gameQuery.platform?.id,
   ordering: gameQuery.sortOrder,
+  search: gameQuery.searchText,
 }} , [gameQuery]);
 
 export default useGames;
